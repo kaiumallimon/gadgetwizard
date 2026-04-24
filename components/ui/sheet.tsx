a58a46577273db9410cbbd9d5b98e13783ed5022
Kaium Al Limon <limon.3cs@gmail.com>
2026-04-25 05:27:06 +0600

    feat: update SheetContent button size from 'icon-sm' to 'icon'

--- components/ui/sheet.tsx
+++ components/ui/sheet.tsx
@@ -88,7 +88,7 @@ function SheetContent({
               <Button
                 variant="ghost"
                 className="absolute top-4 right-4"
-                size="icon-sm"
+                size="icon"
               />
             }
           >
